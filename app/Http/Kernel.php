3933b6e01c5b50638e6c6c3b0b58e7fb7f3d3e1c
Bellie Joe M. Jandusay <jandusayjoe14@gmail.com>
2022-03-27 12:53:37 +0800

    Added CheckAdminRole middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'role' => \App\Http\Middleware\CheckUserRole::class,
+        'admin_role' => \App\Http\Middleware\CheckAdminRole::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
---
+++ app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if(Auth::user()->admin_role != $role){
+            abort(403);
+        }else{
+            return $next($request);
+        }
+    }
+}
